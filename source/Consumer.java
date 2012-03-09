96a7967fc8f34bb41941cfa45258af720f1e427d
jbreitenbucher <jon@breitenbucher.net>
2012-03-08 23:13:52 -0500

    Spring cleanup
    Deleted the ifxetex files as they are now part of full TeXLive install.
    Updated woosterthesis to use xetex option in hyper ref, require the
    mathspec package when using xetex, fix cover length unit to be 1pt
    instead of 1px, and updated username.tex to indicate how to use MLA
    style.

--- source/Consumer.java
+++ source/Consumer.java
@@ -12,7 +12,7 @@ public class Consumer implements Runnable
     public void run() {
         while ( true ) {
             String message = producer.getMessage();
-            System.out.println("Got message: " + message);
+            System.out.println("Got message: "+ message);
 			try { 
 				Thread.sleep( 2000 ); 
 			} catch ( InterruptedException e ) { }
